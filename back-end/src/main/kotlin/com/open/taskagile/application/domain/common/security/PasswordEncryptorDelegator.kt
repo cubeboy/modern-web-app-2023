bc2cd3a147ffae03caf6ff939720755bd97c9fe1
cubeboy@itfact.co.kr <cubeboy@itfact.co.kr>
2024-03-08 11:04:29 +0900

    Append AuthenticationManager on SecurityConfiguration

--- back-end/src/main/kotlin/com/open/taskagile/application/domain/common/security/PasswordEncryptorDelegator.kt
+++ back-end/src/main/kotlin/com/open/taskagile/application/domain/common/security/PasswordEncryptorDelegator.kt
@@ -1,13 +1,14 @@
 package com.open.taskagile.application.domain.common.security
 
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 
 @Component
-class PasswordEncryptorDelegator: PasswordEncryptor {
+class PasswordEncryptorDelegator(val passwordEncoder:PasswordEncoder): PasswordEncryptor {
   //TODO 암호화 구현체 필요
   override fun encrypt(rawPassword: String): Mono<String> {
-    return rawPassword.toMono()
+    return passwordEncoder.encode(rawPassword).toMono()
   }
 }
--- back-end/src/main/kotlin/com/open/taskagile/configuration/SecurityConfiguration.kt
+++ back-end/src/main/kotlin/com/open/taskagile/configuration/SecurityConfiguration.kt
@@ -1,13 +1,25 @@
 package com.open.taskagile.configuration
 
+import com.open.taskagile.infra.repository.UserRepository
 import com.open.taskagile.web.authenticate.JwtTokenAuthenticationFilter
 import com.open.taskagile.web.authenticate.JwtTokenProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.ReactiveAuthenticationManager
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager
+import org.springframework.security.authorization.AuthorizationDecision
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.authorization.AuthorizationContext
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository
+import reactor.core.publisher.Mono
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,16 +29,61 @@ class SecurityConfiguration {
   }
 
   @Bean
-  fun springWebFilterChain(http:ServerHttpSecurity, tokenProvider: JwtTokenProvider): SecurityWebFilterChain {
+  fun springWebFilterChain(
+    http:ServerHttpSecurity,
+    tokenProvider: JwtTokenProvider,
+    reactiveAuthenticationManager: ReactiveAuthenticationManager
+  ): SecurityWebFilterChain {
     return http
       .csrf{ it.disable() }
       .httpBasic { it.disable() }
+      .authenticationManager(reactiveAuthenticationManager)
+      .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
       .authorizeExchange { exchange ->
         exchange
           .pathMatchers(*PUBLIC).permitAll()
-          .anyExchange().permitAll()
+          .pathMatchers("/me").authenticated()
+          .pathMatchers("/users/{user}/**").access(this::currentUserMatcherPath)
+          .anyExchange().authenticated()
       }
       .addFilterAt(JwtTokenAuthenticationFilter(tokenProvider), SecurityWebFiltersOrder.HTTP_BASIC)
       .build()
+  }
+
+  private fun currentUserMatcherPath(
+    authentication: Mono<Authentication>,
+    context:AuthorizationContext
+  ): Mono<AuthorizationDecision> {
+    return authentication
+      .map { context.variables["user"]?.equals(it.name) ?: false }
+      .map { AuthorizationDecision(it) }
+  }
+
+  @Bean
+  fun userDetailsService(users:UserRepository): ReactiveUserDetailsService {
+    return ReactiveUserDetailsService { username ->
+      users.findByUsernameOrEmailAddress(username, "")
+        .map {
+          User
+            .withUsername(it.username)
+            .password(it.password)
+            .build()
+        }
+    }
+  }
+
+  @Bean
+  fun reactiveAuthenticationManager(
+    userDetailsService: ReactiveUserDetailsService,
+    passwordEncoder:PasswordEncoder
+  ): ReactiveAuthenticationManager {
+    val authenticationManager = UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService)
+    authenticationManager.setPasswordEncoder(passwordEncoder)
+    return authenticationManager
+  }
+
+  @Bean
+  fun passwordEncoder(): PasswordEncoder {
+    return BCryptPasswordEncoder()
   }
 }
